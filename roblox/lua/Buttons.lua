0a256aedc6d97f4f180890ec70e767734a2ca9b1
LInKenDEv <dogsaternos@gmail.com>
2025-06-13 18:09:26 +0800

    Update Buttons.lua

--- roblox/lua/Buttons.lua
+++ roblox/lua/Buttons.lua
@@ -1,7 +1,7 @@
 local MyButton = _G.MainTab:CreateButton({
     Name = "Infinite Yield",
     Callback = function()
-        loadstring(game:HttpGet("https://mamao-five.vercel.app/api/Hub/V1/scripts=infiniteYield"))()
+        loadstring(game:HttpGet("https://mamao-five.vercel.app/api/Hub/V1/scripts?name=infiniteYield"))()
     end,
     Flag = "owasa"
 })
