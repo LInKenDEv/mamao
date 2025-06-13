4fa62b60cdf661628021847dd86a52fd19e04888
LInKenDEv <dogsaternos@gmail.com>
2025-06-13 16:25:45 +0800

    Update LoaderV1.js

--- api/Hub/V1/LoaderV1.js
+++ api/Hub/V1/LoaderV1.js
@@ -6,8 +6,6 @@ export default function handler(req, res) {
   const accept = req.headers['accept'] || "";
   const referer = req.headers['referer'] || "";
   const origin = req.headers['origin'] || "";
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-
   const isRoblox = ua.includes("Roblox") && accept.includes("*/*");
   const isFromWeb = referer || origin;
 
@@ -16,13 +14,24 @@ export default function handler(req, res) {
     return res.send(`warn("Access Denied: You are not authorized to use this script.")`);
   }
 
-  const filePath = join(process.cwd(), 'roblox', 'lua', 'loader.lua');
+  const base = join(process.cwd(), "roblox", "lua");
+  const filesInOrder = [
+    "window.lua",
+    "tabs.lua",
+    "sections.lua",
+    "dividers.lua",
+    "toggles.lua",
+    "visibility.lua"
+  ];
 
   try {
-    const obfuscatedCode = readFileSync(filePath, 'utf-8');
+    const combinedLua = filesInOrder
+      .map(file => readFileSync(join(base, file), "utf8"))
+      .join("\n");
+
     res.setHeader("Content-Type", "text/plain");
-    res.status(200).send(obfuscatedCode);
+    res.status(200).send(combinedLua);
   } catch (err) {
-    res.status(500).send("Script loading failed.");
+    res.status(500).send("-- Script loading failed.");
   }
 }
