e4ecda5d3b7e7bd6cf4946aa3d32a835b372772c
LInKenDEv <dogsaternos@gmail.com>
2025-06-13 16:39:43 +0800

    Update loader.lua

--- roblox/lua/loader.lua
+++ roblox/lua/loader.lua
@@ -3,11 +3,11 @@ local base = "https://mamao-five.vercel.app/api/Hub/V1/"
 
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
-_G.Window = loadstring(game:HttpGet(base .. "/Window.js"))()
-_G.MainTab = loadstring(game:HttpGet(base .. "/Tabs.js"))()
-loadstring(game:HttpGet(base .. "/Sections.js"))()
-loadstring(game:HttpGet(base .. "/Dividers.js"))()
-loadstring(game:HttpGet(base .. "/Toggles.js"))()
-loadstring(game:HttpGet(base .. "/Visibility.js"))()
+_G.Window = loadstring(game:HttpGet(base .. "/Window"))()
+_G.MainTab = loadstring(game:HttpGet(base .. "/Tabs"))()
+loadstring(game:HttpGet(base .. "/Sections"))()
+loadstring(game:HttpGet(base .. "/Dividers"))()
+loadstring(game:HttpGet(base .. "/Toggles"))()
+loadstring(game:HttpGet(base .. "/Visibility"))()
 
 -- Rayfield:LoadConfiguration() should be inside one of the scripts (probably visibility.lua)
