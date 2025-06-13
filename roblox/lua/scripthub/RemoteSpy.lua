dab371edcb5ced9c4429db716f3fbcdd166db596
LInKenDEv <dogsaternos@gmail.com>
2025-06-13 19:57:00 +0800

    Update RemoteSpy.lua

--- roblox/lua/scripthub/RemoteSpy.lua
+++ roblox/lua/scripthub/RemoteSpy.lua
@@ -1,3 +1,3 @@
-// loadstring(game:HttpGet("https://raw.githubusercontent.com/Turtle-Brand/Turtle-Spy/main/source.lua", true))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Turtle-Brand/Turtle-Spy/main/source.lua", true))()
 
 warn('disabled')
