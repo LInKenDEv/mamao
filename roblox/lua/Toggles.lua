4c1624b03c52c0110618176f18f1dcc360cdff7e
LInKenDEv <dogsaternos@gmail.com>
2025-06-13 17:50:11 +0800

    Update Toggles.lua

--- roblox/lua/Toggles.lua
+++ roblox/lua/Toggles.lua
@@ -1,9 +1,1 @@
--- Add a Toggle to the tab
-local MyToggle = _G.MainTab:CreateToggle({
-    Name = "Example Toggle",
-    CurrentValue = false,
-    Flag = "ExampleToggle",
-    Callback = function(Value)
-        print("Toggle is now:", Value)
-    end,
-})
+warn("[⚠️ Notice] Toggles has been disabled")
