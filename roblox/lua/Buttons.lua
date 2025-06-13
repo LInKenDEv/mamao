7d850ac02dd8ecc6977bc7bb816cdf6f3dfd976f
LInKenDEv <dogsaternos@gmail.com>
2025-06-13 19:47:51 +0800

    Update Buttons.lua

--- roblox/lua/Buttons.lua
+++ roblox/lua/Buttons.lua
@@ -5,3 +5,10 @@ local MyButton = _G.MainTab:CreateButton({
     end,
     Flag = "owasa"
 })
+
+_G.MainTab:CreateButton({
+	Name = "RemoteSpy",
+	Callback = function()
+		loadstring(game:HttpGet("https://mamao-five.vercel.app/api/Hub/V1/scripts?name=RemoteSpy"))()
+	end,
+})
