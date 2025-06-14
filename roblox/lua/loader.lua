161699251f485ae8abaffedd5b8cdc44336c9688
LInKenDEv <dogsaternos@gmail.com>
2025-06-14 16:19:22 +0800

    Update loader.lua

--- roblox/lua/loader.lua
+++ roblox/lua/loader.lua
@@ -1,17 +1,3 @@
 -- Main loader script in Roblox
 local base = "https://mamao-five.vercel.app/api/Hub/V1/"
-
--- Load and execute Window script (this sets up _G.Window and _G.Rayfield)
-loadstring(game:HttpGet(base .. "Window"))()
-
--- Load and execute Tabs script (this sets up _G.MainTab)
-loadstring(game:HttpGet(base .. "Tabs"))()
-
--- Load other components
-loadstring(game:HttpGet(base .. "Sections"))()
-loadstring(game:HttpGet(base .. "Dividers"))()
-loadstring(game:HttpGet(base .. "Toggles"))()
-loadstring(game:HttpGet(base .. "Button"))()
-loadstring(game:HttpGet(base .. "Visibility"))()
-
--- Rayfield:LoadConfiguration() should be inside one of the scripts (probably Visibility.lua)
+loadstring(game:HttpGet(base .. "Init"))()
