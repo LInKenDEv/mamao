a6acde13bb0adb4f16cc90c1b416ade23dd168f2
LInKenDEv <dogsaternos@gmail.com>
2025-06-14 16:20:03 +0800

    Update Init.js

--- api/Hub/V1/Init.js
+++ api/Hub/V1/Init.js
@@ -1,1 +1,25 @@
+const { readFileSync } = require("fs");
+const { join } = require("path");
 
+module.exports = function handler(req, res) {
+  const ua = req.headers['user-agent'] || "";
+  const accept = req.headers['accept'] || "";
+  const referer = req.headers['referer'] || "";
+  const origin = req.headers['origin'] || "";
+  const isRoblox = ua.includes("Roblox") && accept.includes("*/*");
+  const isFromWeb = referer || origin;
+
+  if (!isRoblox || isFromWeb) {
+    res.setHeader("Content-Type", "text/plain");
+    return res.send(`warn("Access Denied: You are not authorized to use this script.")`);
+  }
+
+  const filePath = join(process.cwd(), 'roblox', 'lua', 'Init.lua');
+  try {
+    const code = readFileSync(filePath, 'utf-8');
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send(code);
+  } catch (err) {
+    res.status(500).send("Script loading failed.");
+  }
+};
