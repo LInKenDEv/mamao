49a0546f0e572c7919a3caf04d288e9180d15fc1
LInKenDEv <dogsaternos@gmail.com>
2025-06-14 16:20:59 +0800

    Create Init.lua

---
+++ roblox/lua/Init.lua
@@ -0,0 +1,9 @@
+local base = "https://mamao-five.vercel.app/api/Hub/V1/"
+loadstring(game:HttpGet(base .. "Window"))()
+loadstring(game:HttpGet(base .. "Tabs"))()
+loadstring(game:HttpGet(base .. "Sections"))()
+loadstring(game:HttpGet(base .. "Dividers"))()
+loadstring(game:HttpGet(base .. "Toggles"))()
+loadstring(game:HttpGet(base .. "Button"))()
+loadstring(game:HttpGet(base .. "Visibility"))()
+-- Rayfield:LoadConfiguration()
